cce1e2c5b4697ef430c84ba57cf63ac71a36d09c
WSlatton <williamslatton11@gmail.com>
2019-09-22 02:43:48 -0500

    hooked up toggle and percent field

--- src/content/content.js
+++ src/content/content.js
@@ -1,5 +1,3 @@
-const percent = 1000;
-
 // parse float from text after removing all non-numeric characters
 function parseNumber(string) {
     // don't parse fields like ($0.17 / count)
@@ -13,6 +11,7 @@ function priceToStrings(price) {
     let whole = Math.floor(price);
     let fraction = Math.floor(100 * (price - whole)).toString().padStart(2, '0');
     let text = '$' + whole.toLocaleString() + '.' + fraction;
+
     return {
         whole: whole,
         fraction: fraction,
@@ -30,56 +29,83 @@ function tooltipText(price, newPrice) {
         + '</span></strong> = ' + priceToString(newPrice);
 }
 
-function adjustPrice(originalPrice) {
+function adjustPrice(percent, originalPrice) {
     return originalPrice * (1 + percent / 100);
+}
+
+function setText(element, text) {
+    if (!element.hasAttribute('original')) {
+        element.setAttribute('original', element.innerText);
+    }
+    
+    element.innerText = text;
+}
+
+function getOriginalText(element) {
+    if (element.hasAttribute('original')) {
+        return element.getAttribute('original');
+    } else {
+        return element.innerText;
+    }
 }
 
 let totalDonation;
 
-function updatePrices() {
+function setTooltip(element, content) {
+    if (element.tooltip !== undefined) {
+        element.tooltip.setContent(content);
+    } else {
+        let tooltip = tippy(element, {
+            content: content,
+            arrow: true
+        });
+        element.tooltip = tooltip;
+    }
+}
+
+let donateButton;
+
+function updatePrices(percent) {
     // prices displayed in searches, on homepage, in suggestions, etc.
     for (priceElement of document.getElementsByClassName('a-price')) {
         // skip text price elements (original price shown crossed out when discounted)
         if (priceElement.classList.contains('a-text-price'))
             continue;
 
-        let price = parseNumber(priceElement.getElementsByClassName('a-offscreen')[0].innerText);
+        let price = parseNumber(getOriginalText(priceElement.getElementsByClassName('a-offscreen')[0]));
 
         if (isNaN(NaN)) {
-            let whole = parseNumber(priceElement.getElementsByClassName('a-price-whole')[0].innerText);
-            let fraction = parseNumber(priceElement.getElementsByClassName('a-price-fraction')[0].innerText);
+            let whole = parseNumber(getOriginalText(priceElement.getElementsByClassName('a-price-whole')[0]));
+            let fraction = parseNumber(getOriginalText(priceElement.getElementsByClassName('a-price-fraction')[0]));
             price = whole + fraction / 100;
         }
         
-        let newPrice = adjustPrice(price);
+        let newPrice = adjustPrice(percent, price);
         let priceStrings = priceToStrings(newPrice);
-        priceElement.getElementsByClassName('a-offscreen')[0].innerText = priceStrings.text;
-        priceElement.getElementsByClassName('a-price-whole')[0].innerText = priceStrings.whole;
-        priceElement.getElementsByClassName('a-price-fraction')[0].innerText = priceStrings.fraction;
+        setText(priceElement.getElementsByClassName('a-offscreen')[0], priceStrings.text);
+        setText(priceElement.getElementsByClassName('a-price-whole')[0], priceStrings.whole);
+        setText(priceElement.getElementsByClassName('a-price-fraction')[0], priceStrings.fraction);
 
-        tippy(priceElement, {
-            content: tooltipText(price, newPrice),
-            arrow: true
-        });
+        setTooltip(priceElement, tooltipText(price, newPrice));
     }
 
     // cart prices
     for (priceElement of document.getElementsByClassName('a-color-price')) {
-        let price = parseNumber(priceElement.innerText);
+        let price = parseNumber(getOriginalText(priceElement));
 
         if (isNaN(price))
             continue;
 
-        let newPrice = adjustPrice(price);
-        priceElement.innerText = priceToString(newPrice);
+        let newPrice = adjustPrice(percent, price);
+        setText(priceElement, priceToString(newPrice));
 
-        tippy(priceElement, {
-            content: tooltipText(price, newPrice),
-            arrow: true
-        });
+        setTooltip(priceElement, tooltipText(price, newPrice));
 
         if (priceElement.classList.contains('grand-total-price')) {
             totalDonation = priceToString(newPrice - price);
+            if (donateButton !== undefined) {
+                donateButton.innerText = 'donate (' + totalDonation + ')';
+            }
         }
     }
 
@@ -106,7 +132,7 @@ function doCheckout() {
     let checkoutButton = document.querySelector('#subtotals > div > div > div > .place-order-button');
     let checkoutButtonDiv = document.querySelector('#subtotals > div > div > div:first-child');
     let buttonsDiv = document.querySelector('#subtotals > div > div:first-child');
-    let donateButton = document.createElement('button');
+    donateButton = document.createElement('button');
 
     donateButton.innerText = 'donate (' + totalDonation + ')';
     donateButton.style = 'padding: 0.5em';
@@ -126,6 +152,23 @@ function doCheckout() {
 }
 
 window.onload = function() {
-    updatePrices();
-    doCheckout();
+    chrome.storage.local.get(['enabled', 'percent'], function(result) {
+        if (result.enabled) {
+            doCheckout();
+            if (result.percent === undefined) {
+                updatePrices(1000);
+            } else {
+                updatePrices(result.percent);
+            }
+        }
+    });
 }
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    if ('enabled' in changes) {
+        window.location.reload('false');
+    }
+    if ('percent' in changes) {
+        updatePrices(changes.percent.newValue);
+    }
+  });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,25 @@
-document.getElementById('form').onsubmit = 'alert("test")';
+window.onload = function() {
+    let toggle = document.getElementById('toggle');
+
+    chrome.storage.local.get(['enabled'], function(result) {
+        toggle.checked = result.enabled;
+
+        toggle.onclick = function() {
+            let enabled = toggle.checked;
+            console.log('enabled = ' + enabled);
+            chrome.storage.local.set({enabled: enabled});
+        };
+    });
+
+    let percent = document.getElementById('percent');
+
+    chrome.storage.local.get(['percent'], function(result) {
+        percent.value = result.percent;
+
+        percent.onkeypress = function(event) {
+            if (event.keyCode == 13 || event.which == 13) {
+                chrome.storage.local.set({ percent: parseFloat(percent.value) });
+            }
+        }
+    });
+}
